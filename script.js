28639ade809e9b84965c8843b6e3a7eb559da544
Aravinthk95 <107410327+Aravinthk95@users.noreply.github.com>
2022-10-18 21:31:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+//  let a = (2);
+//  let b = (7);
+//  const c = (a+b);
+//  console.log(c);
+
+
+//  let u = ("HelloGuvi");
+//  let t = ("Geek");
+//  let e = (u+t);
+//  console.log(e);
+
+
+//  let h = 5;
+//  let j=(h*2);
+//  console.log(j);
+
+//  let v = +"hello";
+//  let y = 10;
+//  let p =(v+y);
+//  console.log(p);
+
+
+// let r =[ 1,2,3,4,5,6,7,8,9,10,11];
+// for (var i = 1; i<=11; i++) {
+// console.log(i);
+// };
+
+
+let r =[ 1,2,3,4,5,6,7,8,9,10,11];
+for (var i =length-1 ; i>=11; i--) {
+console.log(i);
+};
